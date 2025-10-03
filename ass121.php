a8a93d0ed6a6c3ee9fb29cb3361486de4990a830
Bigthap <hangdongbigc2@Gmail.com>
2025-10-03 12:58:59 +0700

    Add files via upload

---
+++ ass121.php
@@ -0,0 +1,19 @@
+<?php
+    $num = [15, 40, 60, 12, 34];
+    $max = 0;
+    $index = 0;
+    $number = count($num);
+
+    // for($i = 0;$i <= $num;$i++){
+    //     if($num[$i] > $num[$i+1]){
+    //         $max = $num[$i];
+    //         $index = $i;
+    //     }
+    // }
+    // for($j = 0;$j <= $num;$j++){
+    //     echo "$num[$j]<br>";
+    // }
+    echo "$number";
+    echo "<br>MAX number = $max";
+    echo "<br>Index = $index";
+?>
